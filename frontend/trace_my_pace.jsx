80dc0a5f638fb626dc64e48f2db183758d16b94a
Elijah Nam <elijah.nam@gmail.com>
2020-04-06 14:16:11 -0400

    Completed Skeleton

---
+++ frontend/trace_my_pace.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // const store = configureStore();
+
+    //TESTING
+
+    //TESTING
+
+    const root = document.getElementById('root');
+    ReactDOM.render(<h1> Page renders </h1>, root);
+});
