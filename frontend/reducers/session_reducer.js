4fc01c478f6bf9d63b1ea2c9d39b9c16443a5d46
Elijah Nam <elijah.nam@gmail.com>
2020-04-21 02:44:33 -0400

    bugs fix attemp 1

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -8,7 +8,8 @@ const sessionReducer = (state = _nullUser, action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            return Object.assign({}, state, {id: action.currentUser.id });
+            // return Object.assign({}, state, {id: action.currentUser.id });
+            return { id: action.currentUser.id };
         case LOGOUT_CURRENT_USER:
             return _nullUser;
         default:
