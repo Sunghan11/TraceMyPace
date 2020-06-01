4e48593e76dbe8a335cbdddfc47e55347834a1b9
Elijah Nam <elijah.nam@gmail.com>
2020-06-01 01:56:48 -0400

    Updated Seed User Files

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,12 +23,13 @@ demoUser = User.create(
 
 users = User.create([
     {first_name: "Demo", last_name: "User", email: "testUser@gmail.com", password:"password", birth_date:"2000-12-20", gender: "Male", location: "United States"},
-    {first_name: "Kobe", last_name: "Bryant", email: "kobe@gmail.com", password:"password", birth_date:"1980-05-12", gender: "Male", location: "United States"},
-    {first_name: "Stephen", last_name: "Curry", email: "scurry@gmail.com", password:"password", birth_date:"2000-12-20", gender: "Male", location: "United States"},
-    {first_name: "Shaquille", last_name: "O'Neal", email: "shaq@gmail.com", password:"password", birth_date:"2000-12-20", gender: "Male", location: "United States"},
-    {first_name: "Lebron", last_name: "James", email: "LB23@gmail.com", password:"password", birth_date:"2000-12-20", gender: "Male", location: "United States"},
-    {first_name: "Michael", last_name: "Jordan", email: "theGOAT@gmail.com", password:"password", birth_date:"2000-12-20", gender: "Male", location: "United States"},
-    {first_name: "Derrick", last_name: "Rose", email: "DRose@gmail.com", password:"password", birth_date:"2000-12-20", gender: "Male", location: "United States"},
+    {first_name: "Kobe", last_name: "Bryant", email: "kobe@gmail.com", password:"password", birth_date:"1978-08-23", gender: "Male", location: "United States"},
+    {first_name: "Stephen", last_name: "Curry", email: "scurry@gmail.com", password:"password", birth_date:"1988-03-14", gender: "Male", location: "United States"},
+    {first_name: "Shaquille", last_name: "O'Neal", email: "shaq@gmail.com", password:"password", birth_date:"1972-03-06", gender: "Male", location: "United States"},
+    {first_name: "Lebron", last_name: "James", email: "LB23@gmail.com", password:"password", birth_date:"1984-12-30", gender: "Male", location: "United States"},
+    {first_name: "Michael", last_name: "Jordan", email: "theGOAT@gmail.com", password:"password", birth_date:"1963-02-17", gender: "Male", location: "United States"},
+    {first_name: "Derrick", last_name: "Rose", email: "DRose@gmail.com", password:"password", birth_date:"1988-10-04", gender: "Male", location: "United States"},
+    {first_name: "Kawhi", last_name: "Leonard", email: "TheClaw@gmail.com", password:"password", birth_date:"1991-06-29", gender: "Male", location: "United States"},
 ])
 
 routes = Route.create ([
