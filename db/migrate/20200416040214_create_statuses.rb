8e9e5084fb874fa178417636ccda00020ec87cc8
Elijah Nam <elijah.nam@gmail.com>
2020-06-11 22:29:38 -0400

    fixed author in table

--- db/migrate/20200416040214_create_statuses.rb
+++ db/migrate/20200416040214_create_statuses.rb
@@ -2,7 +2,7 @@ class CreateStatuses < ActiveRecord::Migration[5.2]
   def change
     create_table :statuses do |t|
       t.string :body, null: false
-      t.integer :author, null: false
+      t.integer :author_id, null: false
 
       t.timestamps
     end
